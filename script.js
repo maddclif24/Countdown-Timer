656b4c1b07635930461885cf64a9b2e53c8f9650
maddclif <maddclif24@gmail.com>
2020-10-07 17:24:03 +0300

    create new rep

---
+++ script.js
@@ -0,0 +1,27 @@
+const newYears = '1 Jan 2021';
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.textContent = formatTime(days);
+    hoursEl.textContent = formatTime(hours);
+    minutesEl.textContent = formatTime(minutes);
+    secondEl.textContent = formatTime(seconds);
+}
+
+const formatTime = (time) => time < 10 ? (`0${time}`) : time;
+
+setInterval(countdown, 1000);
